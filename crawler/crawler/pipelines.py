08652eb9a6e26e092906c5158c6594ddccb7ef96
tarpetos <hevlichtaras@gmail.com>
2022-10-12 15:48:28 +0300

    Add database

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,13 +1,22 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import sqlite3
 
 
 class CrawlerPipeline:
+
+    def __init__(self):
+        self.con = sqlite3.connect('result.db')
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS stored_data(
+            score VARCHAR(10),
+            occupation VARCHAR(100),
+            code VARCHAR(25)
+        )""")
+
     def process_item(self, item, spider):
+        self.cur.execute("""INSERT OR IGNORE INTO stored_data VALUES (?,?,?)""",
+                         (item['score'], item['occupation'], item['code']))
+        self.con.commit()
         return item
